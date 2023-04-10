03693c266f16e73dc6244409a8224c05cb83667b
ArnavBansalCodes <89875865+ArnavBansalCodes@users.noreply.github.com>
2023-04-10 17:04:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function startClasssification() {
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/milUdXkY3/', modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+};
